d009113c31a6350c95f65c32c669233c4e850d92
StringTek <StringTek@foxmail.com>
2020-06-05 15:44:12 +0800

    add DistributedLock and DelayQueue

--- src/main/java/online/stringtek/redis/usecase/DelayQueue.java
+++ src/main/java/online/stringtek/redis/usecase/DelayQueue.java
@@ -1,7 +1,67 @@
 package online.stringtek.redis.usecase;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import javafx.concurrent.Task;
+import redis.clients.jedis.Jedis;
+
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * 延时队列
  * */
-public class DelayQueue {
+public class DelayQueue<T> {
+    private Jedis jedis;
+    private String key;
+    private Type taskType=new TypeReference<TaskItem<T>>(){}.getType();
+    public DelayQueue(Jedis jedis,String key){
+        this.jedis=jedis;
+        this.key=key;
+    }
+    private static class TaskItem<T>{
+        public String id;
+        public T msg;
+    }
+
+    public void enqueue(T msg){
+        TaskItem<T> taskItem=new TaskItem<>();
+        taskItem.id= UUID.randomUUID().toString();
+        taskItem.msg=msg;
+        String json=JSON.toJSONString(taskItem);
+        //放入延时队列延时1s处理
+        jedis.zadd(key,System.currentTimeMillis()+1000,json);
+    }
+    public void loop(){
+        while(!Thread.interrupted()){
+            //取一个任务处理
+            Set<String> items=jedis.zrangeByScore(key,0,System.currentTimeMillis(),0,1);
+            if(items.isEmpty()){
+                //当前队列中无任务
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    //传递interrupt 也可以直接break
+                    Thread.currentThread().interrupt();
+                }
+                continue;
+            }
+            String value=items.iterator().next();
+            //这之间可能有多个线程拿到这个item,可以通过lua脚本来实现一个原子操作来将获取和remove的过程合并在一起
+            if(jedis.zrem(key,value)>1){
+                try{
+                    TaskItem<T> taskItem=JSON.parseObject(value,taskType);
+                    handleMsg(taskItem.msg);
+                }catch (Exception e){
+                    //这里catch住是为了避免一个任务有问题从而影响别的任务
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private void handleMsg(T msg){
+        System.out.println(msg);
+    }
+
 }
--- src/main/java/online/stringtek/redis/usecase/DistributedLock.java
+++ src/main/java/online/stringtek/redis/usecase/DistributedLock.java
@@ -1,8 +1,73 @@
 package online.stringtek.redis.usecase;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 分布式锁
+ * 分布式锁--可重入锁
+ * 暂时没有对redis锁过期的问题做处理
  * */
+
+@Component
 public class DistributedLock {
+    private final ThreadLocal<Map<String,Integer>> currentLockers=new ThreadLocal<>();
+
+    private Jedis jedis;
+    public DistributedLock(Jedis jedis){
+        this.jedis=jedis;
+    }
+    private boolean _lock(String key){
+        //使用原子指令，同时设置值和过期时间
+        return jedis.set(key,"",new SetParams().nx().ex(5))!=null;
+    }
+    public void _unlock(String key){
+        jedis.del(key);
+    }
+    private Map<String,Integer> currentLockers(){
+        Map<String,Integer> map=currentLockers.get();
+        if(map!=null)
+            return map;
+        currentLockers.set(new HashMap<>());
+        return currentLockers.get();
+    }
+    public boolean lock(String key){
+        Map<String,Integer> ref=currentLockers();
+        Integer cnt=ref.get(key);
+        if(cnt!=null){
+            //说明当前线程已经获取到了锁，他想重入
+            ref.put(key,cnt+1);
+            return true;
+        }
+        boolean ok=_lock(key);
+        if(!ok){
+            //抢锁失败
+            return false;
+        }
+        //枪锁成功
+        ref.put(key,1);
+        return true;
+    }
+    public boolean unlock(String key){
+        Map<String,Integer> ref=currentLockers();
+        Integer cnt=ref.get(key);
+        if(cnt==null){
+            //没有获取到锁，更不用说解锁
+            return false;
+        }
+        cnt--;
+        if(cnt==0){
+            //此时可以remove
+            ref.remove(key);
+            _unlock(key);
+        }else{
+            ref.put(key,cnt);
+        }
+        return true;
+    }
 }
